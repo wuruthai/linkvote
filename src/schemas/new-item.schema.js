932b7d7baa66d32a102f8578622c7e912dbb47aa
Onur İrken <onur.irken@bulutmd.com>
2020-11-20 18:44:22 +0300

    - Add favicon - Change head title on that on the browser

--- src/schemas/new-item.schema.js
+++ src/schemas/new-item.schema.js
@@ -2,8 +2,8 @@ import * as yup from 'yup';
 import baseSchema from './base.schema';
 
 const newItemSchema = yup.object().shape({
-  name: baseSchema.name,
-  uri: yup.string().required(),
+  name: baseSchema.name.label('Link Name'),
+  uri: yup.string().url().required().label('Link URL'),
 });
 
 export default newItemSchema;
